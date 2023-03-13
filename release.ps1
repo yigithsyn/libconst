2633f7194e50f3d1e9a4a56bb800e47d16e621d4
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2023-03-13 13:34:28 +0300

    fix repo

--- release.ps1
+++ release.ps1
@@ -1,5 +1,5 @@
 # Use this Powershell script to push releases
-$version = "v1.0.0" 
-$commit  = "initial stable release"
+$version = "v1.0.1" 
+$commit  = "fix repo"
 Write-Output $version > version.tag 
 git add . && git commit -m "$commit" && git push origin && gh release create $version --title "$version" --notes "$commit"
